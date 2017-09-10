2298f605d73bf9452904b28489ce5b6a96d6ea79
gerritvanos <gerrit.vanos@student.hu.nl>
2017-09-10 15:35:28 +0200

    alle opdrachen

---
+++ boek opdrachten/3.17.py
@@ -0,0 +1,10 @@
+#a
+print(eval('2*3+1'))
+#b
+# geeft error omdat het geen getallen zijn print(eval('hello'))
+#C
+print(eval("'hell' + ' ' + 'world!'"))
+#D
+print(eval("'ASCII'.count('I')"))
+#E
+# geeft error ivm aanhalingstekens print(eval('x = 5'))
